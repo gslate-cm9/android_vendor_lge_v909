09defa465518bdea0517bfb2b3967270e10c8b69
Janne Grunau <j@jannau.net>
2012-08-11 15:04:37 +0200

    Do not use binary files for EGL/GLES which are built in aosp

--- v909-vendor-blobs.mk
+++ v909-vendor-blobs.mk
@@ -86,17 +86,12 @@ PRODUCT_COPY_FILES += \
     vendor/lge/v909/proprietary/etc/firmware/nvmm_wmaprodec.axf:system/etc/firmware/nvmm_wmaprodec.axf \
     vendor/lge/v909/proprietary/etc/firmware/nvrm_avp.bin:system/etc/firmware/nvrm_avp.bin \
     vendor/lge/v909/proprietary/lib/egl/libEGL_tegra.so:system/lib/egl/libEGL_tegra.so \
-    vendor/lge/v909/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/lge/v909/proprietary/lib/egl/libEGL_tegra.so:system/lib/egl/libEGL_tegra.so \
     vendor/lge/v909/proprietary/lib/egl/libGLESv1_CM_tegra.so:system/lib/egl/libGLESv1_CM_tegra.so \
     vendor/lge/v909/proprietary/lib/egl/libGLESv2_tegra.so:system/lib/egl/libGLESv2_tegra.so \
     vendor/lge/v909/proprietary/lib/hw/camera.tegra.so:system/lib/hw/camera.tegra.so \
     vendor/lge/v909/proprietary/lib/hw/gralloc.tegra.so:system/lib/hw/gralloc.tegra.so \
     vendor/lge/v909/proprietary/lib/hw/hwcomposer.tegra.so:system/lib/hw/hwcomposer.tegra.so \
-    vendor/lge/v909/proprietary/lib/libEGL.so:system/lib/libEGL.so \
-    vendor/lge/v909/proprietary/lib/libGLESv1_CM.so:system/lib/libGLESv1_CM.so \
-    vendor/lge/v909/proprietary/lib/libGLESv2.so:system/lib/libGLESv2.so \
-    vendor/lge/v909/proprietary/lib/libGLESv2_dbg.so:system/lib/libGLESv2_dbg.so \
     vendor/lge/v909/proprietary/lib/libardrv_dynamic.so:system/lib/libardrv_dynamic.so \
     vendor/lge/v909/proprietary/lib/libcgdrv.so:system/lib/libcgdrv.so \
     vendor/lge/v909/proprietary/lib/libnvapputil.so:system/lib/libnvapputil.so \
